63611e0cd28d781c78919e91101f0c6a1e323e48
mahdisRahmati <mahdisrahmatiii1383@gmail.com>
2025-07-24 23:26:02 +0430

    feat: step 2 - add compressed backup creation using tar.gz

--- backup.sh
+++ backup.sh
@@ -6,3 +6,17 @@ read -p "Enter the file extension to include (example txt or jpg): " file_ext
 echo "Searching for *.$file_ext files in $source_path ..."
 find "$source_path" -type f -name "*.$file_ext" > backup.conf
 echo "List of files saved to backup.conf."
+
+# step 2
+read -p "Enter destination path to save the backup: " dest_path
+timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
+backup_file="backup-$timestamp.tar.gz"
+full_path="$dest_path/$backup_file"
+echo "Creating backup archive at: $full_path"
+tar -czf "$full_path" -T backup.conf
+# Check success
+if [ $? -eq 0 ]; then
+  echo "Backup archive created successfully"
+else
+  echo "Error occurred while creating backup archive."
+fi
